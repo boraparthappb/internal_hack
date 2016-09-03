0932429215fc1bdb3af1ff856dd9415007b2ebfb
nayandas417 <nayan.das@reverieinc.com>
2016-09-03 15:53:32 +0530

    Added Job Search Page

--- NaukriHack/app/src/main/java/com/reverie/naukrihack/JobsForYou.java
+++ NaukriHack/app/src/main/java/com/reverie/naukrihack/JobsForYou.java
@@ -1,7 +1,13 @@
 package com.reverie.naukrihack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class JobsForYou extends AppCompatActivity {
 
@@ -9,5 +15,41 @@ public class JobsForYou extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs_for_you);
+        ImageView cancel = (ImageView)findViewById(R.id.cancelJobSearch);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+
+    private class JobSearchAdapter extends RecyclerView.Adapter<JobSearchAdapter.JobViewHolder>{
+        public class JobViewHolder extends RecyclerView.ViewHolder {
+            public TextView title, org, exp, keySkill;
+            public JobViewHolder(View v){
+                super(v);
+            }
+        }
+
+
+        @Override
+        public JobSearchAdapter.JobViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.jobsearch_cell, parent, false);
+            return new JobViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(JobSearchAdapter.JobViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
     }
 }
