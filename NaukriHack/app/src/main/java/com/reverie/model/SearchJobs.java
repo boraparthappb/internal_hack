21b1ec6c5bfad0f7c584f3cff1e84cfb7bc4ca48
Partha Bora <partha.bora@reverieinc.com>
2016-09-05 10:09:31 +0530

    few jobs added

--- NaukriHack/app/src/main/java/com/reverie/model/SearchJobs.java
+++ NaukriHack/app/src/main/java/com/reverie/model/SearchJobs.java
@@ -8,7 +8,7 @@ public class SearchJobs {
     public String jobOrg;
     public String jobLoc;
     public String jobKeySkills;
-    public static String jobsData[][] = new String[10][5];
+    public static String jobsData[][] = new String[11][5];
     static {
         jobsData[0][0] = "Software Engineer";
         jobsData[0][1] = "BangBit Technologies Private Limited";
@@ -45,5 +45,35 @@ public class SearchJobs {
         jobsData[5][2] = "0-4 yrs";
         jobsData[5][3] = "Bengaluru, Mumbai, Chennai, Pune, Hyderabad, Delhi NCR, Ahmedabad, Kolkata, All India";
         jobsData[5][4] = "PHP, .NET, dot net, vb, Fresher, Java, ASP.Net, C#, SQL Server, HTML, J2Ee";
+
+        jobsData[6][0] = "Investment Banking Analyst";
+        jobsData[6][1] = "BankerBay (Window West Technologies Pvt. Ltd.)";
+        jobsData[6][2] = "1-4 yrs";
+        jobsData[6][3] = "Bengaluru";
+        jobsData[6][4] = "Project Finance, Private Equity, Financial Modelling, Investment Banking...";
+
+        jobsData[7][0] = "Oppournity for Vlsi/ Embedded Recruiter - Mindlance Technologies";
+        jobsData[7][1] = "ml Outsourcing Services Pvt. Ltd.";
+        jobsData[7][2] = "1-4 yrs";
+        jobsData[7][3] = "Bengaluru";
+        jobsData[7][4] = "vlsi, semiconductor, Recruitment, embedded, c++, end to end recruitment";
+
+        jobsData[8][0] = "Air-hostess/cabin Crew Freshers/expd Leading Airline";
+        jobsData[8][1] = "Mukul Consultants India Pvt. Ltd.";
+        jobsData[8][2] = "1-4 yrs";
+        jobsData[8][3] = "Bengaluru";
+        jobsData[8][4] = "Cabin Crew Activities, air hostess activities, sr. cabin crew activities...";
+
+        jobsData[9][0] = "Job Opportunity for Linguists_amazon Hyderabad & Bangalore    ";
+        jobsData[9][1] = "Amazon Development Centre (India) Pvt. Ltd.";
+        jobsData[9][2] = "1-4 yrs";
+        jobsData[9][3] = "Hyderabad, Bengaluru";
+        jobsData[9][4] = "french,  chinese,  Italian, german";
+
+        jobsData[10][0] = "Assistant  Relationship Manager- America Express- Call 9899261491    ";
+        jobsData[10][1] = "American Express India";
+        jobsData[10][2] = "0-4 yrs";
+        jobsData[10][3] = "Hyderabad, Bengaluru";
+        jobsData[10][4] = "Cold Calling, B2C, Lead Generation, Client Acquisition...";
     }
 }
