81d75a7ff71d33e099fff2e746591b8d3fde6c05
nayandas417 <nayan.das@reverieinc.com>
2016-09-05 16:19:11 +0530

    Add Job Details and Localization

---
+++ NaukriHack/app/src/main/java/com/reverie/model/JobDetailsData.java
@@ -0,0 +1,47 @@
+package com.reverie.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by NayanJyoti on 05-09-2016.
+ */
+
+public class JobDetailsData {
+    public static ArrayList<String> jobDetailsStrings = new ArrayList<>();
+    static {
+        jobDetailsStrings.add("Job Details");
+        jobDetailsStrings.add("Software Professional- Java/ J2ee");
+        jobDetailsStrings.add("Global Hunt India Pvt. Ltd.");
+        jobDetailsStrings.add("3 - 8 Years");
+        jobDetailsStrings.add("Bengaluru/Bangalore, Pune, Noida");
+        jobDetailsStrings.add("Posted on 05 Sep, 2016");
+        jobDetailsStrings.add("Job Description");
+        jobDetailsStrings.add("Experience MUST in Core Java, Web services, JMS technology");
+        jobDetailsStrings.add("Industry");
+        jobDetailsStrings.add("IT-Software / Software Services");
+        jobDetailsStrings.add("Functional Area");
+        jobDetailsStrings.add("IT Software- Application Programming, Maintenance");
+        jobDetailsStrings.add("Role");
+        jobDetailsStrings.add("Software Developer");
+        jobDetailsStrings.add("Key Skills");
+        jobDetailsStrings.add("Java Web Services, JMS, J2Ee, Core Java");
+        jobDetailsStrings.add("Role Category");
+        jobDetailsStrings.add("Programming and Design");
+        jobDetailsStrings.add("Desired Candidate profile");
+        jobDetailsStrings.add("UG: B.Tech/B.E. - Any Specialization, BCA - Computers, B.Sc - Any Specialization, Computers, PG:M.Tech - Any Specialization, MCA - Computers, Doctorate:Any Doctorate - Any Specialization, Doctorate Not Required");
+        jobDetailsStrings.add("Excellent knowledge of Relational Databases, SQL and ORM technologies (JPA2, Hibernate)\n" +
+                "Experience in the Spring Framework\n" +
+                "Experience in developing web applications using at least one popular web framework (JSF, jQuery, Spring MVC)\n" +
+                "Experience with test-driven development");
+        jobDetailsStrings.add("Employer Details");
+        jobDetailsStrings.add("Company Name");
+        jobDetailsStrings.add("Global Hunt India Pvt. Ltd.");
+        jobDetailsStrings.add("Company Websites");
+        jobDetailsStrings.add("Not Mentioned");
+        jobDetailsStrings.add("View Contact Details");
+        jobDetailsStrings.add("Send me Jobs Like This");
+        jobDetailsStrings.add("Similar Jobs");
+        jobDetailsStrings.add("Apply");
+        System.out.println(jobDetailsStrings.size());
+    }
+}
--- NaukriHack/app/src/main/java/com/reverie/naukrihack/JobDetails.java
+++ NaukriHack/app/src/main/java/com/reverie/naukrihack/JobDetails.java
@@ -1,25 +1,17 @@
 package com.reverie.naukrihack;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.reverie.model.SearchJobs;
+import com.reverie.model.JobDetailsData;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,120 +27,33 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class JobsForYou extends AppCompatActivity {
-    private SearchJobs[] searchJobsList;
+public class JobDetails extends AppCompatActivity {
+
     private HashMap<String, String> stringEngHindMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jobs_for_you);
-        ImageView cancel = (ImageView)findViewById(R.id.cancelJobSearch);
-        stringEngHindMap = new HashMap<>();
-        cancel.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_job_details);
+        ImageView close = (ImageView)findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.jobListRV);
-        JobSearchAdapter hAdapter = new JobSearchAdapter(getApplicationContext(),false);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(hAdapter);
-        hAdapter.notifyDataSetChanged();
-        if(UserDetails.isHindi){
-            ArrayList<String> inString = new ArrayList<>();
-            for(int i=0;i<11/*SearchJobs.jobsData.length*/;i++){
-                for (int j = 0;j<SearchJobs.jobsData[i].length;j++){
-                    inString.add(SearchJobs.jobsData[i][j]);
-                }
-            }
-            new LocalizationAsync(JobsForYou.this,inString,"english","hindi").execute();
-        }
-    }
-
-    public void localizePage(){
-        if(UserDetails.isHindi){
-            TextView jobTitle = (TextView)findViewById(R.id.jobTitle);
-            jobTitle.setText(stringEngHindMap.get("Jobs for You"));
-        }
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.jobListRV);
-        JobSearchAdapter hAdapter = new JobSearchAdapter(getApplicationContext(), UserDetails.isHindi);
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(hAdapter);
-        recyclerView.setOnClickListener(new View.OnClickListener() {
+        Button apply = (Button)findViewById(R.id.apply);
+        apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(JobsForYou.this,JobDetails.class);
-                startActivity(i);
+                finish();
             }
         });
-        hAdapter.notifyDataSetChanged();
-    }
-
-    public class JobSearchAdapter extends RecyclerView.Adapter<JobSearchAdapter.JobViewHolder>{
-        private boolean isHindi = false;
-        public JobSearchAdapter(Context applicationContext,boolean isHindi) {
-            this.isHindi = isHindi;
+        if(UserDetails.isHindi){
+            stringEngHindMap = new HashMap<>();
+            new LocalizationAsync(this, JobDetailsData.jobDetailsStrings,"english","hindi").execute();
         }
-
-        public class JobViewHolder extends RecyclerView.ViewHolder {
-            private TextView title, org, loc,exp, keySkill;
-            public JobViewHolder(View v){
-                super(v);
-                v.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent i = new Intent(JobsForYou.this,JobDetails.class);
-                        startActivity(i);
-                    }
-                });
-                title = (TextView)v.findViewById(R.id.jobTitle);
-                org = (TextView)v.findViewById(R.id.jobOrg);
-                loc = (TextView)v.findViewById(R.id.jobLoc);
-                exp = (TextView)v.findViewById(R.id.jobExp);
-                keySkill = (TextView)v.findViewById(R.id.jobSkill);
-            }
-        }
-
-
-        @Override
-        public JobSearchAdapter.JobViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.jobsearch_cell, parent, false);
-            return new JobViewHolder(itemView);
-        }
-        @Override
-        public void onBindViewHolder(JobSearchAdapter.JobViewHolder holder, int position) {
-            ScaleAnimation animation = new ScaleAnimation(0.7f,1f,0.7f,1f,Animation.RELATIVE_TO_PARENT,0.5f, Animation.RELATIVE_TO_PARENT,0.5f);
-            animation.setDuration(100);
-            if(isHindi && stringEngHindMap.size()>0){
-                holder.title.setText(stringEngHindMap.get(SearchJobs.jobsData[position][0]));
-                holder.org.setText(stringEngHindMap.get(SearchJobs.jobsData[position][1]));
-                holder.exp.setText(stringEngHindMap.get(SearchJobs.jobsData[position][2]));
-                holder.loc.setText(stringEngHindMap.get(SearchJobs.jobsData[position][3]));
-                holder.keySkill.setText(stringEngHindMap.get(SearchJobs.jobsData[position][4]));
-            }
-            else {
-                holder.title.setText(SearchJobs.jobsData[position][0]);
-                holder.org.setText(SearchJobs.jobsData[position][1]);
-                holder.exp.setText(SearchJobs.jobsData[position][2]);
-                holder.loc.setText(SearchJobs.jobsData[position][3]);
-                holder.keySkill.setText(SearchJobs.jobsData[position][4]);
-            }
-
-        }
-        @Override
-        public int getItemCount() {
-            return 11;
-        }
-    }
-
-    class LocalizationAsync extends AsyncTask<Void, Void, String> {
+    }class LocalizationAsync extends AsyncTask<Void, Void, String> {
         private HttpURLConnection urlConnection;
         private Activity mContext;
         private String targetLang;
@@ -227,12 +132,10 @@ public class JobsForYou extends AppCompatActivity {
                     Log.e("Localization", builder.toString());
                     return builder.toString();
                 } else{
-                    UserDetails.isHindi = false;
                     return null;
                 }
             }
             catch (Exception e) {
-                UserDetails.isHindi = false;
                 Log.e("Exception","="+e);
                 return null;
             } finally {
@@ -244,7 +147,7 @@ public class JobsForYou extends AppCompatActivity {
         protected void onPostExecute(String s) {
             Log.e("Output","="+s);
             if(stringEngHindMap.isEmpty()){
-               stringEngHindMap.clear();
+                stringEngHindMap.clear();
             }
             if (s != null) {
                 JSONArray arr;
@@ -258,15 +161,14 @@ public class JobsForYou extends AppCompatActivity {
                         JSONObject ob = arr.getJSONObject(i);
                         stringEngHindMap.put(ob.getString("inString"), ob.getString("outString"));
                     }
-                    UserDetails.isHindi = true;
                 } catch (JSONException e) {
-                    UserDetails.isHindi = false;
                     e.printStackTrace();
+                    return;
                 }
             }
             else {
                 Toast.makeText(mContext,"Unable to Localize",Toast.LENGTH_SHORT).show();
-                UserDetails.isHindi = false;
+                return;
             }
             localizePage();
         }
@@ -285,5 +187,75 @@ public class JobsForYou extends AppCompatActivity {
             }
             return jObj;
         }
+    }
+
+    private void localizePage() {
+        for (String key:stringEngHindMap.keySet()){
+            System.out.println(key+" "+stringEngHindMap.get(key));
+        }
+
+        TextView title = (TextView)findViewById(R.id.jobDetailsTitle);
+        title.setText(stringEngHindMap.get("Job Details"));
+        TextView jobtitle = (TextView)findViewById(R.id.jobDetailsjobTitle);
+        jobtitle.setText(stringEngHindMap.get("Software Professional- Java/ J2ee"));
+        TextView jobOrg = (TextView)findViewById(R.id.jobDetailsOrg);
+        jobOrg.setText(stringEngHindMap.get("Global Hunt India Pvt. Ltd."));
+        TextView jobExp = (TextView)findViewById(R.id.jobDetailsExp);
+        jobExp.setText(stringEngHindMap.get("3 - 8 Years"));
+        TextView jobDet = (TextView) findViewById(R.id.jobDetailsLoc);
+        jobDet.setText(stringEngHindMap.get("Bengaluru/Bangalore, Pune, Noida"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsPostedOn);
+        jobDet.setText(stringEngHindMap.get("Posted on 05 Sep, 2016"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsDesc);
+        jobDet.setText(stringEngHindMap.get("Job Description"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsDescriptionValue);
+        jobDet.setText(stringEngHindMap.get("Experience MUST in Core Java, Web services, JMS technology"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsIndustry);
+        jobDet.setText(stringEngHindMap.get("Industry"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsIndustryValue);
+        jobDet.setText(stringEngHindMap.get("IT-Software / Software Services"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsFuncArea);
+        jobDet.setText(stringEngHindMap.get("Functional Area"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsFuncAreaValue);
+        jobDet.setText(stringEngHindMap.get("IT Software- Application Programming, Maintenance"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsRole);
+        jobDet.setText(stringEngHindMap.get("Role"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsRoleValue);
+        jobDet.setText(stringEngHindMap.get("Software Developer"));
+        jobDet = (TextView) findViewById(R.id.jobDetailskeySkill);
+        jobDet.setText(stringEngHindMap.get("Key Skills"));
+        jobDet = (TextView) findViewById(R.id.jobDetailskeySkillValue);
+        jobDet.setText(stringEngHindMap.get("Java Web Services, JMS, J2Ee, Core Java"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsRoleCat);
+        jobDet.setText(stringEngHindMap.get("Role Category"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsRoleCatValue);
+        jobDet.setText(stringEngHindMap.get("Programming and Design"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsDesCandPro);
+        jobDet.setText(stringEngHindMap.get("Desired Candidate profile"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsCandEdu);
+        jobDet.setText(stringEngHindMap.get("UG: B.Tech/B.E. - Any Specialization, BCA - Computers, B.Sc - Any Specialization, Computers, PG:M.Tech - Any Specialization, MCA - Computers, Doctorate:Any Doctorate - Any Specialization, Doctorate Not Required"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsCandExp);
+        jobDet.setText(stringEngHindMap.get("Excellent knowledge of Relational Databases, SQL and ORM technologies (JPA2, Hibernate)\n" +
+                "Experience in the Spring Framework\n" +
+                "Experience in developing web applications using at least one popular web framework (JSF, jQuery, Spring MVC)\n" +
+                "Experience with test-driven development"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsEmployerDetails);
+        jobDet.setText(stringEngHindMap.get("Employer Details"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsEmployerDetailsCompName);
+        jobDet.setText(stringEngHindMap.get("Company Name"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsEmployerDetailsCompNameValue);
+        jobDet.setText(stringEngHindMap.get("Global Hunt India Pvt. Ltd."));
+        jobDet = (TextView) findViewById(R.id.jobDetailsEmployerDetailsCompWeb);
+        jobDet.setText(stringEngHindMap.get("Company Websites"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsEmployerDetailsCompWebValue);
+        jobDet.setText(stringEngHindMap.get("Not Mentioned"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsConDetails);
+        jobDet.setText(stringEngHindMap.get("View Contact Details"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsSendjobs);
+        jobDet.setText(stringEngHindMap.get("Send me Jobs Like This"));
+        jobDet = (TextView) findViewById(R.id.jobDetailsSimilarJob);
+        jobDet.setText(stringEngHindMap.get("Similar Jobs"));
+        Button apply = (Button) findViewById(R.id.apply);
+        apply.setText(stringEngHindMap.get("Apply"));
     }
 }
